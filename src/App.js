8270e35a29fb252a25be58d8799a70ffe7dde978
Tejas P Naik <l023tejaspn@gmail.com>
2024-03-21 17:17:54 +0530

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Hero from './components/heroes/hero';
-import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Hero />
+    <div>
+      <BrowserRouter>
+     
+       <Navbar/>
+       <Routes>   
+      <Route path='/' element={<Shop/>}/>
+
+       <Route path='/mens' element={<ShopCategory category="mens"/>}/>
+       <Route path='/Womens' element={<ShopCategory category="womens"/>}/>
+       <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+      <Route path='product' element={<product/>}/>
+      <Route path=':productId' element={<product/>}/>
        
+
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+       </Routes>
       
+      </BrowserRouter> 
     </div>
   );
 }
