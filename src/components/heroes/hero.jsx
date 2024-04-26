79725590edd3b34a7d6fe73f9af494ab80e6a9e4
Tejas P Naik <l023tejaspn@gmail.com>
2024-04-26 13:58:14 +0530

    fixed Image on frontpage

--- src/components/heroes/hero.jsx
+++ src/components/heroes/hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import hand_icon from '../assets/hand_icon.png';
 import hero_image from '../assets/hero_image.png';
-import '../heroes/hero.css' 
+import '../heroes/hero.css';
 
 const Hero = () => {
     return (
@@ -9,18 +9,17 @@ const Hero = () => {
             <div className="hero-left">
                 <h2>New arrivals</h2>
                 <div>
-                <div className="hero-hand-icon ">
-                    <p>JUST</p>
-    
-                   </div>
+                    <div className="hero-hand-icon">
+                    
+                        <p>JUST</p>
+                    </div>
                     <p>FOR</p>
                     <p>YOU</p>
                 </div>
             </div>
-            
             <div className="hero-right">
-                <img src={hero_image} alt="" width={550} height={600} />
-        </div>
+                <img src={hero_image} alt="Hero Image" className="hero-image" />
+            </div>
         </div>
     );
 }
